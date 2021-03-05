b42443fa800f353cb99d6eb8d1bb1904f3bad6a0
Rio <riosinnott@gmail.com>
2021-03-05 07:38:11 -0800

    added functionality

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for
 from flask_mysqldb import MySQL
-from flask_table import Table, Col, LinkCol
+from flask_table import Table, Col, LinkCol, ButtonCol
 
 app = Flask(__name__)
 
@@ -17,11 +17,22 @@ class TaskTable(Table):
     assignedUserID = Col('Assigned User ID')
     frequency = Col('Frequency')
 
+class RewardTable(Table):
+    rewardID=Col('Reward ID')
+    rewardName = Col('Reward Name')
+    points = Col('Points')    
+
+class PendingRewardTable(Table):
+    rewardName = Col('Reward Name')
+    points = Col('Points')
+    approved = ButtonCol('Approve', 'approveReward', url_kwargs=dict(rewardID='rewardID'))
+
 class PendingTaskTable(Table):
     taskName = Col('Task Name')
     points = Col('Points')
     assignedUserID = Col('Assigned User ID')
     frequency = Col('Frequency')
+    approved = ButtonCol('Approve', 'approveTask', url_kwargs=dict(taskID='taskID'))
 
 class Task(object):
     def __init__(self, taskID, taskName, points, active, complete, approved, assignedUserID, createdByUserID, dateCreated, dateCompleted, frequency):
@@ -54,6 +65,36 @@ class Task(object):
         columns = [col[0] for col in cur.description]
         tasks = [dict(zip(columns, row)) for row in cur.fetchall()]
         return tasks
+
+class Reward(object):
+    def __init__(self, rewardID, rewardName, points, redeemed, active, approved, redeemedBy, createdBy):
+        self.rewardID = rewardID
+        self.rewardName = rewardName
+        self.points = points
+        self.redeemed = redeemed
+        self.active = active
+        self.approved = approved
+        self.redeemedBy = redeemedBy
+        self.createdByUserID = createdByUserID
+        self.createdBy = createdBy
+
+    @classmethod
+
+    def get_rewards(cls):
+        cur = mysql.connection.cursor()
+        cur.execute("SELECT * FROM rewards")
+        columns = [col[0] for col in cur.description]
+        rewards = [dict(zip(columns, row)) for row in cur.fetchall()]
+        return rewards
+
+    @classmethod
+
+    def get_rewards_pending_approval(cls):
+        cur = mysql.connection.cursor()
+        cur.execute("SELECT * FROM rewards WHERE approved=1")
+        columns = [col[0] for col in cur.description]
+        rewards = [dict(zip(columns, row)) for row in cur.fetchall()]
+        return rewards
 
 @app.route("/")
 def index():
@@ -89,20 +130,46 @@ def login():
                     return redirect(url_for('user'))
     return render_template('login.html')
 
-@app.route('/admin', methods=['GET', 'POST'])
+
+
+@app.route('/admin')
+
 def admin():
     tasks = Task.get_tasks()
     pendingTasks = Task.get_tasks_pending_approval()
-    print(tasks)
-    print(pendingTasks)
+    pendingRewards = Reward.get_rewards_pending_approval()
+    #print(tasks)
+    #print(pendingTasks)
+    print(pendingRewards)
     taskTable = TaskTable(tasks)
     pendingTaskTable = PendingTaskTable(pendingTasks)
-    return render_template('admin.html', activeTaskTable=taskTable, pendingTaskTable=pendingTaskTable)
+    pendingRewardTable = PendingRewardTable(pendingRewards)
+    return render_template('admin.html', activeTaskTable=taskTable, pendingTaskTable=pendingTaskTable, pendingRewardTable=pendingRewardTable)
     # , pendingTaskTable, pendingRewardTable, activeTaskTable, activeRewardTable, userTable
 
-@app.route('/user', methods=['GET', 'POST'])
-def user():
-    return render_template('user.html', currentPoints, activeTaskTable, pendingTaskTable, completedTaskTable, rewardTable)
+@app.route('/admin/rewardApproval/<rewardID>', methods=['GET', 'POST'])   
+def approveReward(rewardID):
+    # if request.method == "POST":
+    cur = mysql.connection.cursor()
+    r = rewardID
+    print(r)
+    cur.execute("UPDATE rewards SET approved=2 WHERE rewardID=%s", r)
+    mysql.connection.commit()
+    return redirect(url_for('admin'))
+
+@app.route('/admin/taskApproval/<taskID>', methods=['GET', 'POST'])   
+def approveTask(taskID):
+    # if request.method == "POST":
+    cur = mysql.connection.cursor()
+    t = taskID
+    print(t)
+    cur.execute("UPDATE tasks SET approved=2 WHERE taskID=%s", t)
+    mysql.connection.commit()
+    return redirect(url_for('admin'))
+
+#@app.route('/user', methods=['GET', 'POST'])
+#def user():
+    #return render_template('user.html', currentPoints, activeTaskTable, pendingTaskTable, completedTaskTable, rewardTable)
 
 if __name__ == "__main__":
     app.run(host="127.0.0.1", port=8080, debug=True)

d440b2d51ac45b436e57bb02ddd6b9ef86a6c4e8
Rio <riosinnott@gmail.com>
2021-03-20 13:58:47 -0700

    troubleshooting

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from flask_appconfig.heroku import from_heroku_envvars
 import MySQLdb
 from datetime import datetime, timedelta
 import hashlib, random, string, jwt, time, re, os
+from config import ProdConfig
 
 app = Flask(__name__)
 
@@ -23,21 +24,6 @@ login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = "login"
 mail = Mail(app)
-
-class ProdConfig(object):
-    MYSQL_HOST = os.environ.get('MYSQL_HOST')
-    MYSQL_USER = os.environ.get('MYSQL_USER')
-    MYSQL_PASSWORD = os.environ.get('MYSQL_PASSWORD')
-    MYSQL_DB = os.environ.get('MYSQL_DB')
-
-    MAIL_SERVER = os.environ.get('MAIL_SERVER')
-    MAIL_PORT = os.environ.get('MAIL_PORT')
-    MAIL_USE_SSL = os.environ.get('MAIL_USE_SSL')
-    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
-    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    
-    SECRET_KEY = os.environ.get('SECRET_KEY')
-
 
 class User(object):
     def __init__(self, userID, username, displayName, admin, passwordHash, approvalRequired, points, homeID, is_authenticated, is_active, is_anonymous, email):
